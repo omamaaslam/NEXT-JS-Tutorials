84a42a3b7cffc8218fa2f4d9f916e85d0e3528b8
Omama <chohanomama@gmail.com>
2024-05-17 22:20:20 +0500

    React Bootstrap Add

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,42 +1,41 @@
 import './login.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
 const Login = () => {
     return (
-        // <div className="login_card">
-        //     <h4>Login with your cradentials</h4>
-        //     <form id='loginForm'>
-        //         <div>
-        //             <label htmlFor="username">Username</label>
-        //             <input type="text" placeholder='Enter Username' />
-        //         </div>
-        //         <div>
-        //             <label htmlFor="email">Email</label>
-        //             <input type="text" placeholder='Enter Email' />
-        //         </div>
-        //         <button>Login</button>
-        //     </form>
-        // </div>
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+        <Container>
+            <Row className='mt-5 mb-5'>
+                <Col className='col-lg-4 offset-lg-7 col-md-6 offset-md-5 col-12'>
+                    <Card className='p-3'>
+                        <Form>
+                            <div className="mb-3">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </div>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+                            <div className="mb-3">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </div>
+                            <div className="mb-3">
+                                <Form.Check type="checkbox" label="Check me out" />
+                            </div>
+                            <Button type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
